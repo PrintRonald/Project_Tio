afef0daa150d9fc98c38147eeb395cce7b221fa3
PrintRonald <madariaga492@gmail.com>
2022-07-31 23:23:13 -0400

    Project_Tio
    Project_tio

---
+++ js/sendEmail.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+
+async function handleSendEmail(event) {
+    event.preventDefault();
+    const fd = new FormData(this);
+
+    const response = await fetch('https://formspree.io/f/xpznadbp', {
+        method: 'POST',
+        body: fd,
+        headers: {
+            Accept: 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        this.reset()
+        alert('Mensaje enviado')
+    } else {
+        alert('Error al enviar el mensaje')
+    }
+}
+
+form.addEventListener('submit', handleSendEmail);
